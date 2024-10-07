2529e419406e07c75b08effa535aebd76fe9790e
hernadi.balazs <hernadi.balazs@BURG-1E03-14>
2024-10-07 13:28:20 +0200

    aha

--- NapVicce/Controllers/NapVicceController.cs
+++ NapVicce/Controllers/NapVicceController.cs
@@ -15,38 +15,7 @@ namespace NapVicce.Controllers
                 "-Miből építették a trójai falovat? - Kicseszésből.",
                 "-Hány politikus kell egy villanykörte kicseréléséhez? - Kettő. Egyik kicseréli, a másik pedig kiszivárogtatja a sajtónak.",
                 "-Mit hiányol egy FIDESz-es politikus egy értelmiségi vacsoráról? -A meghívást."};
-            string vicc = "";
-            string date = DateTime.Now.DayOfWeek.ToString();
-            if (date == "Monday")
-            {
-                vicc = viccek[0];
-            }
-            else if (date == "Tuesday")
-            {
-                vicc = viccek[1];
-            }
-            else if (date == "Wednesday")
-            {
-                vicc = viccek[2];
-            }
-            else if (date == "Thursday")
-            {
-                vicc = viccek[3];
-            }
-            else if (date == "Friday")
-            {
-                vicc = viccek[4];
-            }
-            else if (date == "Saturday")
-            {
-                vicc = viccek[5];
-            }
-            else if (date == "Sunday")
-            {
-                vicc = viccek[6];
-            }
-
-            return vicc;
+            return viccek[Convert.ToInt32(DateTime.Now.DayOfWeek)-1];
         }
     }
 }
